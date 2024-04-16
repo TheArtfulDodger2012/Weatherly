fde48ad2d07608b9290e98c95ba3ba7ad1423204
Ron Lane <theartfuldodger2012@gmail.com>
2024-04-16 18:51:52 -0400

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Ron Lane on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
